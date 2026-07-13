c113682844b9b9efa7edb78d1dd11cfb18d7fc50
moon-may <katrin-moonmay@yandex.ru>
2026-07-13 21:40:25 +0300

    parser

--- misc/simple_parser.py
+++ misc/simple_parser.py
@@ -5,4 +5,7 @@ url = 'https://quotes.toscrape.com/'
 r = requests.get(url)
 soup = BeautifulSoup(r.text, 'html.parser')
 
-print(soup)
+texts = soup.find_all('span', class_='text')
+
+for one in texts:
+    print(one.text)
